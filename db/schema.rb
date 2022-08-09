99e3701bdf764a24d7b510a133c5a7808cee2ec4
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-08-09 12:05:36 -0400

    Create validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_808_213_129) do # rubocop:disable Metrics/BlockLength: Block has too many lines
+ActiveRecord::Schema[7.0].define(version: 20_220_808_213_129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
