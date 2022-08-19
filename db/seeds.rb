a4817db4cd8957bcaa20afb81a562e3c66e7ff1d
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-08-19 11:47:52 -0400

    Create defult values migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+User.create(name: 'User 3', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+User.create(name: 'User 4', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+User.create(name: 'User 5', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+User.create(name: 'User 6', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+first_post = Post.create(author: first_user, title: 'Hello1', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'Hello2', text: 'This is my second post')
+third_post = Post.create(author: first_user, title: 'Hello3', text: 'This is my third post')
+
+Post.create(author: second_user, title: 'Hello1', text: 'This is my first post')
+Post.create(author: second_user, title: 'Hello2', text: 'This is my second post')
+Post.create(author: second_user, title: 'Hello3', text: 'This is my third post')
+
+Comment.create(post: first_post, author: first_user, text: 'comment 1!' )
+Comment.create(post: first_post, author: first_user, text: 'comment 2!' )
+
+Comment.create(post: second_post, author: first_user, text: 'comment 1!' )
+Comment.create(post: second_post, author: first_user, text: 'comment 2!' )
+
+Comment.create(post: third_post, author: first_user, text: 'comment 1!' )
+Comment.create(post: third_post, author: first_user, text: 'comment 2!' )
+
+Comment.create(post: first_post, author: second_user, text: 'comment 1!' )
+Comment.create(post: first_post, author: second_user, text: 'comment 2!' )
+
+Comment.create(post: second_post, author: second_user, text: 'comment 1!' )
+Comment.create(post: second_post, author: second_user, text: 'comment 2!' )
+
+Comment.create(post: third_post, author: second_user, text: 'comment 1!' )
+Comment.create(post: third_post, author: second_user, text: 'comment 2!' )
