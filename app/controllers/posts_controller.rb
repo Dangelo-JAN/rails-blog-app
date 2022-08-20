ed562e44947798744f3ee4aba10ffb2a08631731
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-08-20 11:11:24 -0400

    fix post index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,9 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = Post.all
-    @post = Post.find(params[:id])
   end
 
-  # def show
-  #   @post = Post.find(params[:id])
-  #   @user = @post.author
-  #   @comments = @post.comments
-  # end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
