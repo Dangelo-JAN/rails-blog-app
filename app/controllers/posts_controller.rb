c8104db6d5baabb905456b3bcf0bec0526000b7b
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-08-19 22:32:40 -0400

    Block with the comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
-  def index; end
-
-  def show
-    puts params
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.all
+    @post = Post.find(params[:id])
   end
+
+  # def show
+  #   @post = Post.find(params[:id])
+  #   @user = @post.author
+  #   @comments = @post.comments
+  # end
 end
