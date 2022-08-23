9ac0c921428d895d9e1067fa1724e3cc8e2681e5
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-08-22 21:34:33 -0400

    Fix n+1 queries

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.all
+    @posts = Post.where(author_id: @user)
   end
 
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comments
+    @comments = @post.comments.includes(:author)
     @user = @post.author
   end
 
