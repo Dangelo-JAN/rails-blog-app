f5238fe7c8cf3c2f865d5026721cd79b343b8ba6
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-08-25 18:10:12 -0500

    Setup Application Controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  devise_scope :user do
+    root 'devise/sessions#new'
+    get '/login', to: 'devise/sessions#new'
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
@@ -8,8 +14,10 @@ Rails.application.routes.draw do
     end
   end
 
+  # redirect to users_index_path toguether with Application_controller's method: after_sign_in_path_for
+  get '/users' => "users#index", :as => :users_root
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
