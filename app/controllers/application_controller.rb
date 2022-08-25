bf825d63cc32df1dc8011c0c4cb501593d2c57ba
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-08-25 19:43:24 -0400

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
@@ -9,7 +9,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
-    # redirect to users_index_path toguether with the root get '/users' => "users#index", :as => :users_root in routes file
+  # redirect to users_index_path toguether with the root
+  # get '/users' => "users#index", :as => :users_root in routes file
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || users_path
   end
